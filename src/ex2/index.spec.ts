a6b2310c871fc3c944edbab6827f4f7fc3092781
Ribeiro Thomas <tribeiro@auchan.fr>
2024-07-25 15:12:27 +0200

    response exercice 2

--- src/ex2/index.spec.ts
+++ src/ex2/index.spec.ts
@@ -1,3 +1,29 @@
-describe("submitOrder", () => {
-  // écrire les tests ici
+import { submitOrder, Order } from './index';
+import { sendOrderEmail } from './lib/email';
+
+/**
+ * Mock de la fonction sendOrderEmail pour éviter l'envoi d'e-mails réels pendant les tests
+ */
+jest.mock('./lib/email', () => ({
+    sendOrderEmail: jest.fn(),
+}));
+
+/**
+ * Tests pour la fonction submitOrder
+ */
+describe("Vérification de la gestion et validation des soumissions de commandes", () => {
+
+    it("Doit marquer la commande comme soumise et appeler sendOrderEmail", () => {
+        const order: Order = { id: '5678', isSubmitted: false };
+        const updateOrder = submitOrder(order);
+
+        expect(updateOrder.isSubmitted).toBe(true);
+        expect(sendOrderEmail).toHaveBeenCalledWith('5678');
+    });
+
+    it("Doit émettre une erreur si la commande est déjà soumise", () => {
+        const order: Order = { id: '5678', isSubmitted: true };
+
+        expect(() => submitOrder(order)).toThrow('La commande est déjà soumise');
+    });
 });
